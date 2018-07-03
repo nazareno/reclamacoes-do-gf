78c8237a7fcb4b8e38529c8757091c567eccac60
RAQUEL VIGOLVINO LOPES <raquel@dsc.ufcg.edu.br>
2018-07-03 11:47:41 -0300

    revisados
    a geração de avaliações já insere dados como range das avaliações e número de avaliadores (antes estavam na analise do erro). Também tem um script para instalar de uma vez todas as bibliotecas do projeto.

---
+++ code/instalacoesRStudio.R
@@ -0,0 +1,10 @@
+install.packages("tidyverse")
+install.packages("tidytext")
+install.packages("here")
+install.packages("lexiconPT")
+install.packages("modelr")
+install.packages("broom")
+install.packages("rvest")
+install.packages("stringr")
+
+
