4d42cddb67d0c7489c81b8d3765a346f9925b05a
Livia Sampaio <livia@computacao.ufcg.edu.br>
2018-10-30 13:25:53 -0300

    Exemplo EDA em sala

---
+++ code/introducao-EDA/geraHtml.R
@@ -0,0 +1,5 @@
+library(rmarkdown)
+
+render("/Users/raquelvl/Documents/raquel/disciplinas/2018.1/MC/aulas/1.eda.Rmd",  output_format = "html_document")
+render("/Users/raquelvl/Documents/raquel/disciplinas/2018.1/MC/aulas/2.eda-visualiza.Rmd",  output_format = "html_document")
+
